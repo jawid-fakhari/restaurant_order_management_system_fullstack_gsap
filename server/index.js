3279a8ac031252001cb4bcb58f5e931ab6deab8f
jawid-fakhari <jawid_fakhari@live.it>
2024-09-04 09:29:20 +0200

    creazione server, product model

--- server/index.js
+++ server/index.js
@@ -2,29 +2,45 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
-import foodRoutes from "./routes/foods.js";
+//importare il modello, si usa in POST request
+import Product from "./models/product.model.js"
 
 const app = express();
 
-app.use('/foods', foodRoutes);
-
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true })); //== app.use(express.json()) // con la differenzza che qui posso controllare il limite.
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
+// come creare GET request
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+// come creare POST request
+app.post("/api/products", async (req, res) => {
+  try {
+    //creare un modello di product nel nostro database
+    const product = await Product.create(req.body);
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+});
+
 const CONNECTION_URL =
-  "mongodb+srv://jawidfakhari:bestRestaurantApp@restaurant-app.duaml.mongodb.net/?retryWrites=true&w=majority&appName=Restaurant-app";
+  "mongodb+srv://jawidfakhari:bestRestaurantApp@restaurant-app.duaml.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Restaurant-app";
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
-.then(() => app.listen(PORT, (err, result) => {
-    if(err) console.log(err);
-    console.log(`Server is running on port ${PORT}`);
-}))
-.catch(err =>{
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, (err, result) => {
+      if (err) console.log(err);
+      console.log(`Server is running on port ${PORT}`);
+    })
+  )
+  .catch((err) => {
     console.log(err);
-});
+  });
---
+++ server/models/product.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+//creare un modello del data per il nostro prodotto
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please entere food's name"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    ingredients: {
+      type: String,
+      required: [true, "Please enter ingridients"],
+    },
+  },
+  {
+    timestamps: true, //ci da due opzione, data del creazione e data del aggiornamento
+  }
+);
+
+//nominare il il nostro ProductSchema che poi andrà nel database
+const Product = mongoose.model("Product", ProductSchema);
+//esportare il modello
+export default Product;
+
+//questo è come si crea un modello, poi useremo questo modello per creare dei prodotti nel nostro databse
