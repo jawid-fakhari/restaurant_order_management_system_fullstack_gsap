ce8251096895d1eab76316c112f7ca8f96ad0b5b
jawid-fakhari <jawid_fakhari@live.it>
2024-09-21 18:01:02 +0200

    styles, admin page, login page, signup page

--- client/src/pages/clienti/Clients.jsx
+++ client/src/pages/clienti/Clients.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Container, Grid2, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom';
 
 
@@ -9,14 +9,12 @@ function Clients() {
     const tableNumbers = 10;
     const navigate = useNavigate();
 
-    const cliclkHandler = (nt) => {
-        // Reindirizza al menu passando il numero del tavolo
-        console.log(nt);
-        
-        navigate(`/menu/${nt}`);
+    const cliclkHandler = (tableNumber) => {
+        // Reindirizza al menu passando il numero del tavolo        
+        navigate(`/menu/${tableNumber}`);
     }
     const tableArray = new Array(tableNumbers).fill().map((tableNumber, i) => {
-        tableNumber = i +1;
+        tableNumber = i + 1;
         return <Box sx={{
             width: 150,
             height: 60,
@@ -27,7 +25,7 @@ function Clients() {
             cursor: 'pointer',
             margin: 5
         }}
-        //funzione il box item rindrizza al callback func con onclick listener
+            //funzione il box item rindrizza al callback func con onclick listener
             onClick={() => cliclkHandler(tableNumber)}
         >
             <Typography variant="h6" component="div" sx={{ marginTop: 2 }}>
@@ -41,7 +39,9 @@ function Clients() {
             <Navbar />
             <Container>
                 <Typography variant='h5' marginBottom={5}>Scegli il tuo tavolo</Typography>
-                {tableArray}
+                <Grid2 container spacing={2}>
+                    {tableArray}
+                </Grid2>
             </Container>
         </>
     )
